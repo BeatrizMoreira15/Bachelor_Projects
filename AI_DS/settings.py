ff5410c2c7200ce12514c590237f8fe08b44bcb3
Beatriz Moreira <up202208293@up.pt>
2026-04-26 18:38:46 +0100

    Reorganize projects

---
+++ AI_DS/settings.py
@@ -0,0 +1,13 @@
+# Window Size
+WIDTH = 700
+HEIGHT = 700
+
+# frame rate
+TICTAC = 30
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+CREME = (250, 235, 215)
+BLUE = (0, 0, 255)
+SALMON = (250, 128, 114)
